670622f76f3da291dcd6c7b5464df8da292c8a97
gitsish <149239698+gitsish@users.noreply.github.com>
2023-10-28 16:13:04 +0530

    Update bot-telegram.py

--- bot-telegram.py
+++ bot-telegram.py
@@ -12,7 +12,7 @@ async def chatgpt(message):
     api_url = "https://api.openai.com/v1/chat/completions"
 
 # Set the API key (replace YOUR_API_KEY with your actual API key)
-    token=""
+    token="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"
     api_key = "Bearer "+token
 
 # Set the request parameters
@@ -47,7 +47,7 @@ async def reply_to_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Response: \n {message_text}")
 
 if __name__ == '__main__':
-    token_string=""
+    token_string="6879831782:AAEGn2vv6_MBhb9kGXr_bS5yuWQ4ENfDRJ8"
     application = ApplicationBuilder().token(token_string).build()
 
     start_handler = CommandHandler('start', start)
